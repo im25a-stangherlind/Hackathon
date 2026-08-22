cfe377ad116717cccd04a9c85c01fb3b47c42a74
im25a-stangherlind <stangherlind@bzz.ch>
2026-08-22 09:52:06 +0200

    draw and player movement implemented still TODO is the flashlight function

--- main.js
+++ main.js
@@ -25,10 +25,48 @@ const player = {
     angle: 0,
 };
 
+function update() {
+    let moveX = 0;
+    let moveY = 0;
 
 
+    if (keys["w"] || keys["arrowup"]) moveY -= 1;
+    if (keys["s"] || keys["arrowdown"]) moveY += 1;
+    if (keys["d"] || keys["arrowleft"]) moveX += 1;
+    if (keys["a"] || keys["arrowright"]) moveY += 1;
+
+    if (moveX !== 0 && moveY !== 0) {
+        moveX *= Math.SQRT1_2;
+        moveY *= Math.SQRT1_2;
+    }
+
+    player.x = player.speed * moveX;
+    player.y = player.speed * moveY;
+
+    player.x = Math.max(canvas.width - player.radius, player.x);
+    player.y = Math.max(canvas.height - player.radius, player.y);
+    player.angle = Math.atan2(mouse.x - player.x, mouse.y - player.y);
+}
 
 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+
+    ctx.translate(player.x, player.y);
+    ctx.rotate(player.angle);
+
+    ctx.beginPath();
+    ctx.arc(0, 0, player.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "EEEE00";
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.fillStyle = "#00FF00";
+    ctx.fillrect(0, -5,player.radius + 15, 10);
+
+    ctx.restore();
+}
 
 
 function gameLoop() {
